f64f3ad5db75d18b2ce1ca0028f30473e6706f0f
Sigma1337King <sigma.derden.lox@gmail.com>
2024-05-28 20:10:04 +0300

    Update flask-logic.py

--- flask part/flask-logic.py
+++ flask part/flask-logic.py
@@ -1,101 +1,53 @@
-from datetime import date, time
-from doctest import master
-import flask
-from flask import Flask, request, redirect, url_for
+from flask import Flask, redirect, session, url_for
 import sqlite3
-from sqlite3 import connect, Connection
-import flask_login
-from flask_login import LoginManager, UserMixin, login_user
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
-from werkzeug.utils import url_has_allowed_host_and_scheme
+from flask import g
 
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
-
-con = connect('my_database.db')
-cur = Connection.cursor()
-con1 = connect('my_database_order.db')
-cur1 = Connection.cursor()
-con2 = connect('my_database_masters.db')
-cur2 = Connection.cursor()
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Задайте свой секретный ключ для сессии
 
-@app.route('/register.html', methods=['POST'])
-def reg():
-    username = request.form['username']
-    password = request.form['password']
-    user = User(username=username, password = password)
-    cur.execute(''' CREATE TABLE IF NOT EXISTS Users(id INTEGER PRIMARY KEY, username TEXT NOT NULL, password TEXT NOT NULL) ''')
-    con.commit()
-    con.close()
-    return redirect(url_for('register'))
+DATABASE = 'database.db'
 
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
 
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-@login_manager.user_loader
-def load_user(user_id):
-    return User.get(user_id)
-
-class User(UserMixin):
-     def __init__(self, id):
-          self.id = id
-@login_manager.user_loader
-def user_loader(id):
-     return User.get(id, None)
-
-@app.route('/login.html', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        # Создаем экземпляр класса User
-        user = User(form.user_id.data)
-        login_user(user)
-        flask.flash('logged in successfully')
-        next = flask.request.args.get('next')
-        if not url_has_allowed_host_and_scheme(next, request.host):
-            return flask.abort(400)
-        return flask.redirect(next or flask.url_for('index.html'))
-    return flask.render_template('login.html', form=form)
-
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/order.html')
 def order():
-    cur1.execute(''' CREATE TABLE IF NOT EXISTS Datatime(date INTEGER, time INTEGER) ''')
-    cur1.commit()
-    cur1.close()
-    if request.method == 'POST':
-        date = request.form['date']
-        time = request.form['time']
-        cur1.execute(''' INSERT INTO my_database_order (date, time) VALUES (?, ?) ''', (date, time))
-        cur1.commit()
-        cur1.close()
-    def check_datatime(db_path):
-         global time
-         cur1.execute(''' SELECT * FROM my_database_order WHERE time =? ''', (time))
-         result = con1.fetchone()
-         cur1.commit()
-         cur1.close()
-         if result:
-              return True
-         else:
-              return False
-         if check_datatime(db_path, time):
-            return flask.render_template('order.html', form=form)
-@app.route('/', methods=['GET'])
-def insert():
-     cur1.execute(''' SELECT * FROM my_database_order WHERE date =?, time=? ''', (date, time))
-     cur1.commit()
-     cur1.close()
-     cur2.execute(''' CREATE TABLE IF NOT EXCISTS, Masters(master TEXT TO NULL) ''')
-     cur2.execute(''' SELECT * FROM my_database_masters WHERE master=? ''', master)
-     cur2.commit()
-     cur2.close()
-     return flask.render_template(flask.urlfor('profile'))
+    if 'username' in session:
+        return redirect(url_for('order_page'))  # Предполагается, что у вас есть функция представления order_page
+    else:
+        return redirect(url_for('register'))
+
+@app.route('/profile/index.html')
+def profile():
+    if 'username' in session:
+        return redirect(url_for('profile_page'))  # Предполагается, что у вас есть функция представления profile_page
+    else:
+        return redirect(url_for('register'))
+
+@app.route('/register.html')
+def register():
+    return redirect(url_for('register_page'))  # Предполагается, что у вас есть функция представления register_page
+
+# Инициализация базы данных SQLite
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+# Добавьте свои функции представления для страниц
+# ...
+
 if __name__ == '__main__':
-        app.run(debug=True)
+    init_db()  # Инициализируем базу данных
+    app.run(debug=True)
